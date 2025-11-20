4865e25a005d153a577469cd4f12e16cec05fc95
potrusaevd <potrusaevd@gmail.com>
2025-11-20 17:27:56 +0300

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -174,7 +174,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- БЛОК 2: ЛОГИКА РАБОТЫ С API ---
     // ==========================================================
     
-    const API_URL = `http://localhost:${.env.PORT || 3001}/api`;
+    const API_URL = 'http://localhost:10000/api';
     
     // Привязываем обработчики отправки форм
     loginFormContainer.querySelector('form').addEventListener('submit', handleLogin);
@@ -277,6 +277,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
 
 
 
